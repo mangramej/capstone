79f5f50c5a9d0badc8446ab2eb55d20b66eb5be9
Jemar Sabocido <jfsabocido@student.fatima.edu.ph>
2023-08-12 18:16:53 +0800

    add address to users table - yajra/laravel-address

---
+++ database/migrations/2023_08_12_094318_add_address_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('street')->nullable();
+            $table->string('barangay_id', 9)->nullable()->index();
+            $table->string('city_id', 6)->nullable()->index();
+            $table->string('province_id', 4)->nullable()->index();
+            $table->string('region_id', 2)->nullable()->index();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'street',
+                'barangay_id',
+                'city_id',
+                'province_id',
+                'region_id',
+            ]);
+        });
+    }
+};
