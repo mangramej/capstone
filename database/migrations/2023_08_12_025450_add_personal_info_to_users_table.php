eba85ebae0c7f5f59f5fa368d12034596ab6285a
Jemar Sabocido <jfsabocido@student.fatima.edu.ph>
2023-08-12 12:00:04 +0800

    add personal info migration to users table

---
+++ database/migrations/2023_08_12_025450_add_personal_info_to_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('date_of_birth')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('sex')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'first_name',
+                'middle_name',
+                'last_name',
+                'date_of_birth',
+                'phone_number',
+                'sex',
+            ]);
+        });
+    }
+};
