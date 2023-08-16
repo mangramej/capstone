6634bb3e48bc81f9247a13e5c184171ab8e150b5
Jemar Sabocido <jfsabocido@student.fatima.edu.ph>
2023-08-16 19:46:55 +0800

    change app logo

--- resources/views/layouts/requester.blade.php
+++ resources/views/layouts/requester.blade.php
@@ -4,69 +4,20 @@
     <header class="bg-gray-50">
         <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
             <div class="flex items-center sm:justify-between sm:gap-4">
-                <div class="relative hidden sm:block">
-                    <label class="sr-only" for="search"> Search </label>
-
-                    <input
-                        class="h-10 w-full rounded-lg border-none bg-white pe-10 ps-4 text-sm shadow-sm sm:w-56"
-                        id="search"
-                        type="search"
-                        placeholder="Search website..."
-                    />
-
-                    <button
-                        type="button"
-                        class="absolute end-1 top-1/2 -translate-y-1/2 rounded-md bg-gray-50 p-2 text-gray-600 transition hover:text-gray-700"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="h-4 w-4"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                            />
-                        </svg>
-                    </button>
+                <div class="relative">
+                    <a href="{{ route('dashboard') }}">
+                        <x-logo size="12"/>
+                    </a>
                 </div>
 
-                <div
-                    class="flex flex-1 items-center gap-8 justify-end"
-                >
+                <div class="flex flex-1 items-center gap-8 justify-end">
                     <div class="flex gap-4">
-                        <button
-                            type="button"
-                            class="block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700 sm:hidden"
-                        >
-                            <span class="sr-only">Search</span>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                stroke-width="2"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                                />
-                            </svg>
-                        </button>
-
                         <button
                             x-data=""
                             x-on:click.prevent="$dispatch('open-modal', 'send_request_modal')"
                             href="#"
                             class="block shrink-0 rounded-lg bg-white px-4 p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
                         >
-                            <span class="sr-only">Academy</span>
                             Send a Request
                         </button>
 
