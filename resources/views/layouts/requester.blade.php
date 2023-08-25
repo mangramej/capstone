219a1d92f2dd762f928a90561cc026a80217aa75
Jemar Sabocido <jfsabocido@student.fatima.edu.ph>
2023-08-25 18:57:18 +0800

    make header section flexible

--- resources/views/layouts/requester.blade.php
+++ resources/views/layouts/requester.blade.php
@@ -22,41 +22,36 @@
                         </button>
 
                         <x-breeze-modal name="send_request_modal">
-                            <livewire:requester.send-milk-request />
+                            <livewire:requester.send-milk-request/>
                         </x-breeze-modal>
 
-
-
-
-
-{{-- NOTIFICATION                       --}}
-{{--                        <a--}}
-{{--                            href="#"--}}
-{{--                            class="block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"--}}
-{{--                        >--}}
-{{--                            <span class="sr-only">Notifications</span>--}}
-{{--                            <svg--}}
-{{--                                xmlns="http://www.w3.org/2000/svg"--}}
-{{--                                class="h-5 w-5"--}}
-{{--                                fill="none"--}}
-{{--                                viewBox="0 0 24 24"--}}
-{{--                                stroke="currentColor"--}}
-{{--                                stroke-width="2"--}}
-{{--                            >--}}
-{{--                                <path--}}
-{{--                                    stroke-linecap="round"--}}
-{{--                                    stroke-linejoin="round"--}}
-{{--                                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"--}}
-{{--                                />--}}
-{{--                            </svg>--}}
-{{--                        </a>--}}
+                        {{--                        <a--}}
+                        {{--                            href="#"--}}
+                        {{--                            class="block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"--}}
+                        {{--                        >--}}
+                        {{--                            <span class="sr-only">Notifications</span>--}}
+                        {{--                            <svg--}}
+                        {{--                                xmlns="http://www.w3.org/2000/svg"--}}
+                        {{--                                class="h-5 w-5"--}}
+                        {{--                                fill="none"--}}
+                        {{--                                viewBox="0 0 24 24"--}}
+                        {{--                                stroke="currentColor"--}}
+                        {{--                                stroke-width="2"--}}
+                        {{--                            >--}}
+                        {{--                                <path--}}
+                        {{--                                    stroke-linecap="round"--}}
+                        {{--                                    stroke-linejoin="round"--}}
+                        {{--                                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"--}}
+                        {{--                                />--}}
+                        {{--                            </svg>--}}
+                        {{--                        </a>--}}
 
                         <div x-data="{ isActive: false }" class="relative w-full">
                             <button
                                 x-on:click="isActive = !isActive"
                                 class="block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
                             >
-                                <x-icon name="cog" class="w-6 h-6" />
+                                <x-icon name="cog" class="w-6 h-6"/>
                             </button>
 
                             <div
@@ -95,21 +90,9 @@
                 </div>
             </div>
 
-            <div class="mt-8">
-                <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">
-                    Welcome!
-                </h1>
-
-                <p class="mt-1.5 text-sm text-gray-500">
-                    Below are the history of your milk requests, along with their status.
-                </p>
-            </div>
+            @yield('header')
         </div>
     </header>
 
     @yield('content')
-
-    @isset($slot)
-        {{ $slot }}
-    @endisset
 @endsection
--- resources/views/requester/dashboard.blade.php
+++ resources/views/requester/dashboard.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.requester')
 
+@section('header')
+    <div class="mt-8">
+        <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">
+            Dashboard
+        </h1>
+
+        <p class="mt-1.5 text-sm text-gray-500">
+            Below are the history of your milk requests, along with their status.
+        </p>
+    </div>
+@endsection
+
 @section('content')
     <section>
         <div class="mx-auto max-w-screen-xl py-8 sm:px-6 lg:px-8">
