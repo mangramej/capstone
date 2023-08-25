9969127b551ec5a8d6125f5220df658cd24fb3f4
Jemar Sabocido <jfsabocido@student.fatima.edu.ph>
2023-08-25 19:55:02 +0800

    add champion nav bar

--- resources/views/champion/dashboard.blade.php
+++ resources/views/champion/dashboard.blade.php
@@ -1,20 +1,7 @@
 @extends('layouts.champion')
 
 @section('content')
-    <section>
-        <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button
-                    type="submit"
-                    class="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm text-red-700 hover:bg-red-50"
-                    role="menuitem"
-                >
-                    Logout
-                </button>
-            </form>
-
-            <h1>Champion</h1>
-        </div>
-    </section>
+    <div>
+        <h1>Dashboard</h1>
+    </div>
 @endsection
--- resources/views/layouts/champion.blade.php
+++ resources/views/layouts/champion.blade.php
@@ -1,9 +1,119 @@
 @extends('layouts.base')
 
 @section('body')
-    @yield('content')
+    <nav x-data="{open: false}" class="bg-white shadow py-1.5">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="relative flex h-16 items-center justify-between">
+                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                    <!-- Mobile menu button-->
+                    <button @click="open = ! open" type="button"
+                            class="relative inline-flex items-center justify-center rounded-md p-2 bg-amber-800 text-white"
+                            aria-controls="mobile-menu" aria-expanded="false">
+                        <span class="absolute -inset-0.5"></span>
+                        <span class="sr-only">Open main menu</span>
+                        <!--
+                          Icon when menu is closed.
 
-    @isset($slot)
-        {{ $slot }}
-    @endisset
+                          Menu open: "hidden", Menu closed: "block"
+                        -->
+                        <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                        </svg>
+                        <!--
+                          Icon when menu is open.
+
+                          Menu open: "block", Menu closed: "hidden"
+                        -->
+                        <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                        </svg>
+                    </button>
+                </div>
+                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                    <div class="flex flex-shrink-0 items-center">
+                        <x-logo />
+                    </div>
+                    <div class="hidden sm:ml-6 sm:block">
+                        <div class="flex space-x-4 items-center h-full">
+                            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                            <a href="#" class="bg-amber-900 text-white rounded-md px-3 py-2 text-sm font-medium transition ease"
+                               aria-current="page">Dashboard</a>
+                            <a href="#"
+                               class="hover:bg-amber-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium transition ease">Milk Bag</a>
+                            <a href="#"
+                               class="hover:bg-amber-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium transition ease">Providers</a>
+                            <a href="#"
+                               class="hover:bg-amber-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium transition ease">Messages</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    <!-- Profile dropdown -->
+                    <div class="relative ml-3" x-data="{profile: false}">
+                        <div>
+                            <button type="button"
+                                    @click="profile = !profile"
+                                    class="relative flex rounded-full text-sm hover:bg-amber-700 hover:text-white transition ease"
+                                    id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                <span class="absolute -inset-1.5"></span>
+                                <span class="sr-only">Open user menu</span>
+                                <x-icon name="cog" class="w-8 h-8"/>
+                            </button>
+                        </div>
+
+                        <!--
+                          Dropdown menu, show/hide based on menu state.
+
+                          Entering: "transition ease-out duration-100"
+                            From: "transform opacity-0 scale-95"
+                            To: "transform opacity-100 scale-100"
+                          Leaving: "transition ease-in duration-75"
+                            From: "transform opacity-100 scale-100"
+                            To: "transform opacity-0 scale-95"
+                        -->
+                        <div
+                            x-show="profile"
+                            x-transition:enter="transition ease-out duration-200"
+                            x-transition:enter-start="opacity-0 scale-95"
+                            x-transition:enter-end="opacity-100 scale-100"
+                            x-transition:leave="transition ease-in duration-75"
+                            x-transition:leave-start="opacity-100 scale-100"
+                            x-transition:leave-end="opacity-0 scale-95"
+                            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                            <!-- Active: "bg-gray-100", Not Active: "" -->
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
+                               id="user-menu-item-0">Your Profile</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
+                               id="user-menu-item-1">Settings</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
+                               id="user-menu-item-2">Sign out</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden" id="mobile-menu">
+            <div class="space-y-1 px-2 pb-3 pt-2">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                <a href="#" class="bg-amber-900 text-white block rounded-md px-3 py-2 text-base font-medium transition ease"
+                   aria-current="page">Dashboard</a>
+                <a href="#"
+                   class="hover:bg-amber-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium transition ease">Milk Bag</a>
+                <a href="#"
+                   class="hover:bg-amber-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium transition ease">Providers</a>
+                <a href="#"
+                   class="hover:bg-amber-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium transition ease">Messages</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="mt-8 mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        @yield('content')
+    </div>
 @endsection
