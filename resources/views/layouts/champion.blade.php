8baa75a54804431b709a4ee7cced9bff504e4e84
Jemar Sabocido <jfsabocido@student.fatima.edu.ph>
2023-08-28 06:33:57 +0800

    auto close champion dropdown when click outside

--- resources/views/layouts/champion.blade.php
+++ resources/views/layouts/champion.blade.php
@@ -82,6 +82,8 @@
                             x-transition:leave="transition ease-in duration-75"
                             x-transition:leave-start="opacity-100 scale-100"
                             x-transition:leave-end="opacity-0 scale-95"
+                            x-on:click.away="profile = false"
+                            x-on:keydown.escape.window="profile = false"
                             class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <!-- Active: "bg-gray-100", Not Active: "" -->
