3dada48cb1fafa50254be559134d27468d6ce0ea
Jemar Sabocido <jfsabocido@student.fatima.edu.ph>
2023-08-30 19:18:06 +0800

    welcome page

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')
-    ->name('home');
+Route::view('/', 'welcome')->name('home');
 
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)
