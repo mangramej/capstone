6e74ed5e08a14facb25fc4a4cef583aaf012b06f
Jemar Sabocido <jfsabocido@student.fatima.edu.ph>
2023-09-30 17:27:23 +0800

    reformat codebase

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
- use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Modules\Castables\PersonalInfo;
 use App\Modules\Enums\UserEnum;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
